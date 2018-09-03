e8c27ac55db2845beec2333ae2c9c2f8467a23c8
huangyan <huangyan@vip.house365.com>
2018-09-03 19:11:08 +0800

    发送录音时展示相关动画

--- NIMLibrary/src/main/java/com/renyu/nimlibrary/ui/view/VoiceRecorderView.java
+++ NIMLibrary/src/main/java/com/renyu/nimlibrary/ui/view/VoiceRecorderView.java
@@ -1,5 +1,6 @@
 package com.renyu.nimlibrary.ui.view;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
@@ -37,22 +38,59 @@ public class VoiceRecorderView extends RelativeLayout implements IAudioRecordCal
 
     private boolean isRecording = false;
 
+    private int maxDuration = 60;
+    private long currentSecond = 0;//当前秒数
+
+    private static final int WHAT_CURRENT_RECORD_DURATION = 11;//当前录音的时间
+    private Runnable timeRunnable;
+    private boolean cancel = false;
+    private boolean isShowTime = false;//显示倒计时
+
     // 图片刷新使用
+    @SuppressLint("HandlerLeak")
     Handler micImageHandler = new Handler() {
+        @SuppressLint("DefaultLocale")
         @Override
         public void handleMessage(android.os.Message msg) {
-            micImage.setImageDrawable(micImages[msg.what]);
+            if (!cancel) {
+                micImage.setImageDrawable(micImages[msg.what]);
+            }
         }
     };
+
+    //计时器
+    @SuppressLint("HandlerLeak")
+    private Handler timeHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == WHAT_CURRENT_RECORD_DURATION) {
+                if (audioMessageHelper.isRecording()) {
+                    long time = (long) msg.obj;
+                    if (time >= maxDuration - 10 && !cancel) {//倒计时10秒
+                        isShowTime = true;
+                        recordingHint.setText(String.format("还能继续说%d秒", maxDuration - time));
+                    } else {
+                        isShowTime = false;
+                    }
+                } else {
+                    timeHandler.removeCallbacks(timeRunnable);
+                }
+
+            }
+        }
+    };
+
 
     // 被点击的View
     View v;
 
     // 录音成功回调接口
     IAudioRecordCallback callback;
+
     public interface IAudioRecordCallback {
         void onRecordSuccess(File audioFile, long audioLength, RecordType recordType);
     }
+
     public void setIAudioRecordCallback(IAudioRecordCallback callback) {
         this.callback = callback;
     }
@@ -77,27 +115,23 @@ public class VoiceRecorderView extends RelativeLayout implements IAudioRecordCal
 
         micImage = findViewById(R.id.mic_image);
         recordingHint = findViewById(R.id.recording_hint);
-
-        micImages = new Drawable[] { getResources().getDrawable(R.mipmap.ease_record_animate_01),
+        micImages = new Drawable[]{getResources().getDrawable(R.mipmap.ease_record_animate_01),
                 getResources().getDrawable(R.mipmap.ease_record_animate_02),
                 getResources().getDrawable(R.mipmap.ease_record_animate_03),
                 getResources().getDrawable(R.mipmap.ease_record_animate_04),
                 getResources().getDrawable(R.mipmap.ease_record_animate_05),
                 getResources().getDrawable(R.mipmap.ease_record_animate_06),
-                getResources().getDrawable(R.mipmap.ease_record_animate_07),
-                getResources().getDrawable(R.mipmap.ease_record_animate_08),
-                getResources().getDrawable(R.mipmap.ease_record_animate_09),
-                getResources().getDrawable(R.mipmap.ease_record_animate_10),
-                getResources().getDrawable(R.mipmap.ease_record_animate_11),
-                getResources().getDrawable(R.mipmap.ease_record_animate_12),
-                getResources().getDrawable(R.mipmap.ease_record_animate_13),
-                getResources().getDrawable(R.mipmap.ease_record_animate_14), };
+                getResources().getDrawable(R.mipmap.ease_record_animate_07)};
 
-        audioMessageHelper = new AudioRecorder(context, RecordType.AAC, 60, this);
+        if (maxDuration < 0) {
+            maxDuration = AudioRecorder.DEFAULT_MAX_AUDIO_RECORD_TIME_SECOND;
+        }
+        audioMessageHelper = new AudioRecorder(context, RecordType.AAC, maxDuration, this);
     }
 
     /**
      * 长按开始录音
+     *
      * @param v
      * @param event
      * @return
@@ -110,20 +144,28 @@ public class VoiceRecorderView extends RelativeLayout implements IAudioRecordCal
                 audioMessageHelper.startRecord();
                 return true;
             case MotionEvent.ACTION_MOVE:
-                if (event.getY() < 0) {
+                if (Math.abs(event.getY()) > 300) {
+                    cancel = true;
                     showReleaseToCancelHint();
                 } else {
-                    showMoveUpToCancelHint();
+                    cancel = false;
+                    if (!isShowTime) {
+                        showMoveUpToCancelHint();
+                    }
                 }
                 return true;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL:
-                boolean cancel = false;
-                if (event.getY() < 0) {
+                if (!cancel && event.getEventTime() - event.getDownTime() < 1000) {
+                    showTooShortHint();
                     cancel = true;
+                    audioMessageHelper.completeRecord(true);
+                    new Handler().postDelayed(() -> stopRecording(v), 1000);
+                } else {
+                    audioMessageHelper.completeRecord(cancel);
+                    stopRecording(v);
                 }
-                stopRecording(v);
-                audioMessageHelper.completeRecord(cancel);
+
                 return true;
             default:
                 audioMessageHelper.completeRecord(true);
@@ -138,7 +180,27 @@ public class VoiceRecorderView extends RelativeLayout implements IAudioRecordCal
 
     @Override
     public void onRecordStart(File audioFile, RecordType recordType) {
+        Log.d("NIM_APP", "onRecordStart");
+        currentSecond = 0;
+        timeRunnable = () -> {
+            try {
+                if (audioMessageHelper.isRecording()) {
+                    currentSecond = currentSecond + 1;
+                    Log.d("onRecordStart", " currentSecond= " + currentSecond);
+                    Message message = timeHandler.obtainMessage();
+                    message.what = WHAT_CURRENT_RECORD_DURATION;
+                    message.obj = currentSecond;
+                    timeHandler.sendMessage(message);
+                    timeHandler.postDelayed(timeRunnable, 1000);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
+        };
+        if (isRecording) {
+            timeHandler.postDelayed(timeRunnable, 1000);
+        }
     }
 
     @Override
@@ -171,8 +233,7 @@ public class VoiceRecorderView extends RelativeLayout implements IAudioRecordCal
         ((Activity) getContext()).getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setVisibility(View.VISIBLE);
-        recordingHint.setText("手指上滑，取消发送");
-        recordingHint.setBackgroundColor(Color.TRANSPARENT);
+        showMoveUpToCancelHint();
         view.setPressed(true);
 
         isRecording = true;
@@ -180,7 +241,7 @@ public class VoiceRecorderView extends RelativeLayout implements IAudioRecordCal
             try {
                 while (isRecording) {
                     Message msg = new Message();
-                    msg.what = audioMessageHelper.getCurrentRecordMaxAmplitude() * 13 / 0x7FFF;
+                    msg.what = audioMessageHelper.getCurrentRecordMaxAmplitude() * 6 / 0x7FFF;
                     micImageHandler.sendMessage(msg);
                     SystemClock.sleep(100);
                 }
@@ -188,6 +249,7 @@ public class VoiceRecorderView extends RelativeLayout implements IAudioRecordCal
                 e.printStackTrace();
             }
         }).start();
+
     }
 
     public void stopRecording(View view) {
@@ -201,13 +263,22 @@ public class VoiceRecorderView extends RelativeLayout implements IAudioRecordCal
     }
 
     public void showReleaseToCancelHint() {
-        recordingHint.setText("松开手指，取消发送");
+        recordingHint.setText("松开手指取消发送");
         recordingHint.setBackgroundResource(R.drawable.ease_recording_text_hint_bg);
+        micImage.setImageResource(R.mipmap.cancel);
     }
 
     public void showMoveUpToCancelHint() {
-        recordingHint.setText("手指上滑，取消发送");
+        recordingHint.setText("手指上滑取消发送");
         recordingHint.setBackgroundColor(Color.TRANSPARENT);
+        int i = audioMessageHelper.getCurrentRecordMaxAmplitude() * 6 / 0x7FFF;
+        micImage.setImageDrawable(micImages[i]);
+    }
+
+    public void showTooShortHint() {
+        recordingHint.setText("说话时间太短");
+        recordingHint.setBackgroundColor(Color.TRANSPARENT);
+        micImage.setImageResource(R.mipmap.voice_to_short);
     }
 
     public void onPause() {
